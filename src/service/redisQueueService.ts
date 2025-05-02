5ee72f86759da9722d6eff553908f64f80215615
Meet Kotadiya <meet@atcults.com>
2025-05-02 06:34:15 +0000

    added redis latency

--- src/service/redisQueueService.ts
+++ src/service/redisQueueService.ts
@@ -3,22 +3,22 @@ import { Env } from '../types/env';
 
 export class StreamConsumer implements DurableObject {
 	redis: Redis;
-    isRunning: boolean;
+	isRunning: boolean;
 	env: Env;
-	lastId:string;
+	lastId: string;
 
-    constructor(state: DurableObjectState, env: Env) {
-		this.env = env;	
-        this.redis = new Redis({
-            url: this.env.UPSTASH_REDIS_URL,
-            token: this.env.UPSTASH_REDIS_TOKEN,
-        });
-        this.isRunning = false;
+	constructor(state: DurableObjectState, env: Env) {
+		this.env = env;
+		this.redis = new Redis({
+			url: this.env.UPSTASH_REDIS_URL,
+			token: this.env.UPSTASH_REDIS_TOKEN,
+		});
+		this.isRunning = false;
 		this.lastId = '0';
-    }
-    async readFromStream() {
-        try {
-            const messages:any = await this.redis.xread(this.env.UPSTASH_REDIS_STREAM_NAME, this.lastId);
+	}
+	async readFromStream() {
+		try {
+			const messages: any = await this.redis.xread(this.env.UPSTASH_REDIS_STREAM_NAME, this.lastId);
 			if (messages && messages.length > 0) {
 				// console.log('New messages:', messages);
 				const [streamName, entries] = messages[0];
@@ -26,51 +26,55 @@ export class StreamConsumer implements DurableObject {
 				for (const [id, keyValueArray] of entries) {
 					const data: Record<string, string> = {};
 					for (let i = 0; i < keyValueArray.length; i += 2) {
-					  const key = keyValueArray[i];
-					  const value = keyValueArray[i + 1];
-					  data[key] = value;
+						const key = keyValueArray[i];
+						const value = keyValueArray[i + 1];
+						data[key] = value;
 					}
-				
+					const [timestampStr] = id.split('-');
+					const timestamp = Number(timestampStr);
+					const now = Date.now();
+					const latency = now - timestamp;
+
+					console.log('Latency (ms):', latency);
 					console.log('ID:', id);
 					console.log('Data:', data);
-					this.lastId = id;  //last id
-				  }
+					this.lastId = id; //last id
+				}
 			} else {
 				console.log('No new messages.');
 			}
-            return messages;
-        } catch (error) {
-            console.log('Error reading from stream', error);
-            return null;
-        }
-    }
+			return messages;
+		} catch (error) {
+			console.log('Error reading from stream', error);
+			return null;
+		}
+	}
 
-    async continuousPolling() {
-        if (this.isRunning) return;
+	async continuousPolling() {
+		if (this.isRunning) return;
 
-        this.isRunning = true;
-        console.log("Starting continuous Redis polling...");
-        while (this.isRunning) {
-            console.log("Reading from Redis stream...");
-            await this.readFromStream();
-            await this.waitFor(2000);  // timeout 
-        }
-    }
+		this.isRunning = true;
+		console.log('Starting continuous Redis polling...');
+		while (this.isRunning) {
+			console.log('Reading from Redis stream...');
+			await this.readFromStream();
+			await this.waitFor(2000); // timeout
+		}
+	}
 
-    // artificial delay
-    waitFor(ms: number) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+	// artificial delay
+	waitFor(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
 
 	//incase we want to setop
-    async stopContinuousPolling() {
-        this.isRunning = false;
-        console.log("Stopped continuous Redis polling.");
-    }
+	async stopContinuousPolling() {
+		this.isRunning = false;
+		console.log('Stopped continuous Redis polling.');
+	}
 
-    
-    async fetch(request: Request) {
-		await this.continuousPolling()
-        return new Response('This Durable Object handles Redis operations.');
-    }
+	async fetch(request: Request) {
+		await this.continuousPolling();
+		return new Response('This Durable Object handles Redis operations.');
+	}
 }
